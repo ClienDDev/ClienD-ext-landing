4983efb9c2e49cbf947f5ffe8991a9763ec31a50
atnartur <i@atnartur.ru>
2019-06-23 14:35:59 +0300

    ! ссылка на расширение

--- js/main.js
+++ js/main.js
@@ -13,34 +13,10 @@ $(document).ready(function(){
     if(typeof yaCounter34344415 !== 'undefined')
         counter = yaCounter34344415;
 
-    $('#chrome_web_store_button').click(function(){
-        counter.reachGoal($(this).attr('id'));
-    })
     if(typeof chrome === 'undefined' || typeof chrome.webstore === 'undefined' || typeof chrome.webstore.install === 'undefined'){
         $('.install_button').click(function(){
             if(confirm('Расширение не поддерживается этим браузером. Скачать Google Chrome?'))
                 window.open('http://google.com/chrome');
-        });
-    }
-    else{
-        $('.install_button').click(function(){
-            var $button = $(this);
-            var first_button_html = $button.html();
-            $button.attr('disabled', 'disabled');
-
-            chrome.webstore.install($('meta[rel="chrome-webstore-item"]').attr('href'), function(res){
-                counter.reachGoal('install');
-                alert('Спасибо за установку расширения!');
-                $button.removeAttr('disabled');
-                $button.html(first_button_html);
-            }, function(res){
-                console.log(res);
-                alert('Установка расширения не удалась. Попробуйте, пожалуйста, еще раз');
-                $button.removeAttr('disabled');
-            });
-            chrome.webstore.onDownloadProgress.addListener(function(percentDownloaded){
-                $button.html('<i class="fa fa-spin fa-spinner"></i> Установка ' + percentDownloaded + '%');
-            });
         });
     }
 
